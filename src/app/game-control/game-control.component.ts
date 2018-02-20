8966bcc7ea0bf8c005b68c15285219245e51c827
Jack <jack.yujia.yuan@gmail.com>
2018-02-20 11:23:56 +0800

    Add an attribute to prevent the bug: ToDo: Can trigger many instances of setInterval() but can only clear one of them.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -9,6 +9,7 @@ export class GameControlComponent implements OnInit {
   // @Output() intervalFired = new EventEmitter<number>();
   @Output() intervalFired: EventEmitter<number> = new EventEmitter();
   interval ;
+  triggered: boolean = false;
   lastNumber = 0;
 
   constructor() { }
@@ -16,16 +17,19 @@ export class GameControlComponent implements OnInit {
   ngOnInit() {
   }
   onStartGame(){
+    if (this.triggered === false){
     this.interval = setInterval(()=> {
       this.intervalFired.emit(this.lastNumber + 1);
       this.lastNumber++;
 
     },1000);
-
+      this.triggered = true;
+    }
 
   }
   onPauseGame(){
     clearInterval(this.interval);
+    this.triggered =false;
 
   }
 
